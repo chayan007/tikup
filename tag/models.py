98978a5c5ff687bef92f9960a9385ee89f7efbc8
chayan007 <sonicxxx7@gmail.com>
2020-07-15 10:26:50 +0530

    Tag models (1/2)

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Hashtag(models.Model):
+    """Model for hashtags."""
+
+    name = models.CharField(max_length=100)
+    views = models.IntegerField(default=0)
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class HashtagLink(models.Model):
+    """Model for hashtags mapping with post."""
+
+    post = 1
+    hashtag = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
