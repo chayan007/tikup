c85c5a1d91d383579689a2d9b0044e907159cadd
chayan007 <sonicxxx7@gmail.com>
2020-07-15 10:49:28 +0530

    Profile boost

--- usermodule/models.py
+++ usermodule/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from base.models import BaseModel
+
+
+class Profile(BaseModel):
+    """One-to-one profile model."""
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    dob = models.DateField(null=True)
+    country = models.CharField(max_length=100, null=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.get_full_name()
