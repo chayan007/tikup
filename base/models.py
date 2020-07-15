ead998b54cc675f4f38445e3e6f9f80f8ec9e7d3
chayan007 <sonicxxx7@gmail.com>
2020-07-15 10:34:52 +0530

    Base Abstract Model for analytics

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
+import uuid as uuid
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """Base model for Tikup infrastructure."""
+
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At', db_index=True)
+    modified_at = models.DateTimeField(auto_now=True, verbose_name='Last Modified At')
+
+    class Meta:
+        """Define meta params for model."""
+
+        abstract = True
+        ordering = ('-created_at',)
