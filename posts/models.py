e5de4f28c8f06d9696b565a4518b667a118cc64f
chayan007 <sonicxxx7@gmail.com>
2020-07-15 10:45:42 +0530

    Post model done

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from base.models import BaseModel
+
+from sounds.models import Sound
+
+from usermodule.models import Profile
+
+
+class Post(BaseModel):
+    """Model for posts."""
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    sound = models.ForeignKey(Sound, on_delete=models.PROTECT)
+    video_file = models.FileField(upload_to='uploads/video/')
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.profile.user.get_full_name()
