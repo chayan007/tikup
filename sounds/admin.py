31a7124853a3039514a4d41806322a84fe8aa3d7
chayan007 <sonicxxx7@gmail.com>
2020-07-25 13:37:49 +0530

    Added util query to sound

--- sounds/admin.py
+++ sounds/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
 from sounds.models import Sound
+from sounds.utils import *
 
-admin.site.register(Sound)
+
+@admin.register(Sound)
+class SoundAdmin(admin.ModelAdmin):
+    """Admin for sound model."""
+
+    def get_profile_photo_url(self, obj):
+        return profile_photo_url(obj)
+
+    def get_video_count(self, obj):
+        return video_count(obj)
+
+    list_display = (
+        'name', 'profile', 'first_video',
+        'get_profile_photo_url',
+        'get_video_count'
+    )
---
+++ sounds/utils.py
@@ -0,0 +1,14 @@
+"""Utility functions for sound."""
+from posts.models import Post
+
+
+def profile_photo_url(sound_obj):
+    """Get profile photo of owner."""
+    return sound_obj.profile.display_pic.url
+
+
+def video_count(sound_obj):
+    """Return number of videos made."""
+    return Post.objects.filter(
+        sound=sound_obj
+    ).count()
