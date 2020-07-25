f0e14f88dd2941db0d5d5af34158e0c52c6f9487
chayan007 <sonicxxx7@gmail.com>
2020-07-25 18:11:46 +0530

    SQL Engine

--- tikup/settings.py
+++ tikup/settings.py
@@ -93,6 +93,14 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'db.cnf',
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
