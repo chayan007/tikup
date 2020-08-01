56ac2451362c86d4d043276567ec0edcd2834e1a
chayan007 <sonicxxx7@gmail.com>
2020-08-01 08:42:37 +0530

    obtain auth token added

--- tikup/urls.py
+++ tikup/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/token/', obtain_auth_token, name='api_token_auth')
 ]
