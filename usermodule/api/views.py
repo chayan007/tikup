05ba5c9379196814671c811509c71efc1ae5aafc
chayan007 <sonicxxx7@gmail.com>
2020-08-02 12:23:34 +0530

    Registration APi

---
+++ usermodule/api/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from usermodule.api.serializers import UserSerializer
+
+
+@api_view(['POST'])
+def create_auth(request):
+    serialized = UserSerializer(data=request.DATA)
+    if serialized.is_valid():
+        User.objects.create_user(
+            serialized.init_data['email'],
+            serialized.init_data['username'],
+            serialized.init_data['password']
+        )
+        return Response(serialized.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serialized._errors, status=status.HTTP_400_BAD_REQUEST)
--- usermodule/urls.py
+++ usermodule/urls.py
@@ -1,9 +1,10 @@
 """Common URLs for usermodule application."""
 from django.urls import path, include
 
-from usermodule.api import router
+from usermodule.api import router, views
 
 
 urlpatterns = [
     path('api/models/', include(router.urls)),
+    path('register/', views.create_auth)
 ]
