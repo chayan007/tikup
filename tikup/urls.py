66ec70679da44a60ba2c2b93e3a9cd03c79bb78e
chayan007 <sonicxxx7@gmail.com>
2020-08-06 22:52:11 +0530

    media urls added

--- tikup/urls.py
+++ tikup/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('users/', include('usermodule.urls'), name='users'),
     path('sounds/', include('sounds.urls'), name='sounds'),
     path('auth/token/', obtain_auth_token, name='api_token_auth')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
