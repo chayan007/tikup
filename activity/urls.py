1f10fcc68e2d1dedfae8a82a6adf6a281924bbf9
chayan007 <sonicxxx7@gmail.com>
2020-08-13 10:10:08 +0530

    Added to API stream

--- activity/urls.py
+++ activity/urls.py
@@ -1,10 +1,10 @@
 """Common URLs for posts application."""
 from django.urls import path
 
-from activity.api.views import PostCommentView
+from activity.api.views import PostCommentView, PostLikeView, PostReportView
 
 urlpatterns = [
     path('api/comment/', PostCommentView.as_view(), name='comment'),
-    path('api/like/', PostCommentView.as_view(), name='like'),
-    path('api/report/', PostCommentView.as_view(), name='report'),
+    path('api/like/', PostLikeView.as_view(), name='like'),
+    path('api/report/', PostReportView.as_view(), name='report'),
 ]
