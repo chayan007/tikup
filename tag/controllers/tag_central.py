efc0207b53d44e80ac44904796128fc0e40f680b
chayan007 <sonicxxx7@gmail.com>
2020-08-22 19:27:15 +0530

    Tag central added

---
+++ tag/controllers/tag_central.py
@@ -0,0 +1,33 @@
+"""Handle all tag related functions."""
+from tag.models import Hashtag, HashtagLink
+
+
+class TagCentral:
+    """Handle all tag functions."""
+
+    def extract_tags(self, post_description):
+        """Extract all tag."""
+        return set(
+            part[1:] for part in post_description.split() if part.startswith('#')
+        )
+
+    def store_all_tags(self, tag):
+        """Store all tags present in post description."""
+        Hashtag.objects.create(
+            name=tag
+        )
+
+    def link_all_tags_to_post(self, tag, post):
+        """Link one tag to a post."""
+        HashtagLink.objects.create(
+            post=post,
+            hashtag__name=tag
+        )
+
+    def handle_tag_cycle(self, post):
+        """Handle all tags."""
+        tags = self.extract_tags(post.description)
+        if tags:
+            for tag in tags:
+                self.store_all_tags(tag)
+                self.link_all_tags_to_post(tag, post)
