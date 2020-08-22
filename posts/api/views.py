3684ce83c7ff5a68718f9dcd5fd0e7e3fd473819
chayan007 <sonicxxx7@gmail.com>
2020-08-22 18:52:13 +0530

    search post

--- posts/api/views.py
+++ posts/api/views.py
@@ -1,10 +1,14 @@
+from django.core.paginator import Paginator
+
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from posts.api.serializers import PostSerializer
 from posts.controllers.uploader import PostUploader
 from posts.exceptions import PostUploadException
+from posts.models import Post
 
 
 class PostUploadView(APIView):
@@ -24,3 +28,27 @@ class PostUploadView(APIView):
             ),
             status=status.HTTP_201_CREATED
         )
+
+
+class PostSearchAPI(APIView):
+    """Search posts."""
+
+    def get(self, request, *args, **kwargs):
+        """Get paginated search results."""
+        page_number = self.request.query_params.get('page_number ', 1)
+        page_size = self.request.query_params.get('page_size ', 10)
+        search_token = self.request.query_params.get('search', None)
+        if not search_token:
+            raise Exception('Search Token not provided.')
+        posts = Post.objects.filter(
+            description__icontains=search_token
+        )
+        paginator = Paginator(posts, page_size)
+        serializer = PostSerializer(
+            paginator.page(page_number),
+            many=True,
+            context={'request': request}
+        )
+        response = Response(serializer.data, status=status.HTTP_200_OK)
+        return response
+
