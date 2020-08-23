c8ab5d8215cd5a6c2fe880d3820d9f9d45d5493f
chayan007 <sonicxxx7@gmail.com>
2020-08-23 13:32:06 +0530

    handle unique tags

--- tag/controllers/tag_central.py
+++ tag/controllers/tag_central.py
@@ -5,19 +5,25 @@ from tag.models import Hashtag, HashtagLink
 class TagCentral:
     """Handle all tag functions."""
 
-    def extract_tags(self, post_description):
+    @staticmethod
+    def extract_tags(post_description):
         """Extract all tag."""
         return set(
             part[1:] for part in post_description.split() if part.startswith('#')
         )
 
-    def store_all_tags(self, tag):
+    @staticmethod
+    def store_tag(tag):
         """Store all tags present in post description."""
-        Hashtag.objects.create(
-            name=tag
-        )
+        try:
+            Hashtag.objects.create(
+                name=tag
+            )
+        except BaseException:
+            return
 
-    def link_all_tags_to_post(self, tag, post):
+    @staticmethod
+    def link_all_tags_to_post(tag, post):
         """Link one tag to a post."""
         HashtagLink.objects.create(
             post=post,
@@ -29,7 +35,7 @@ class TagCentral:
         try:
             tags = self.extract_tags(post.description)
             for tag in tags:
-                self.store_all_tags(tag)
+                self.store_tag(tag)
                 self.link_all_tags_to_post(tag, post)
             return True
         except BaseException as e:
