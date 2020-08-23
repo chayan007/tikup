b1db4ccce8d0e52e323b5043cab8484516e15c09
chayan007 <sonicxxx7@gmail.com>
2020-08-23 13:12:27 +0530

    post issue share bug

--- posts/controllers/uploader.py
+++ posts/controllers/uploader.py
@@ -12,13 +12,16 @@ class PostUploader:
     def upload(user, file, details):
         """Upload a post in model."""
         try:
+            original_post = None
+            if details['share_post_uuid']:
+                original_post = Post.objects.get(uuid=details.get('share_post_uuid'))
             post = Post.objects.create(
                 profile=user.profile,
                 video_file=file['video_file'],
                 video_gif=file['video_gif'],
                 description=details.get('description', None),
                 sound=Sound.objects.get(uuid=details.get('sound_uuid', None)),
-                share_pointer=Post.objects.get(uuid=details.get('share_post_uuid', None)),
+                share_pointer=original_post,
                 category=PostCategory(name=details.get('category', None)),
                 is_pornographic=details.get('is_pornographic', False)
             )
