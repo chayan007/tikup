55ae2ffefd33c7ecd0bc02f28dedb45d49e49960
Chayan Datta <chayandatta007@gmail.com>
2020-08-27 18:33:24 +0530

    Added API documentation

--- settings/base.py
+++ settings/base.py
@@ -43,7 +43,8 @@ PROJECT_APPS = [
 
 THIRD_PARTY_APPS = [
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'drf_yasg'
 ]
 
 INSTALLED_APPS = [
@@ -71,6 +72,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 
 TEMPLATES = [
