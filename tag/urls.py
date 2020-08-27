edcf1d104ac35b65f48a35a2dd7c0e7252e1f91b
Chayan Datta <chayandatta007@gmail.com>
2020-08-27 18:50:04 +0530

    url typo

--- tag/urls.py
+++ tag/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 from tag.api.views import HashtagSearchView
 
 urlpatterns = [
-    path('/api/search', HashtagSearchView.as_view(), name='search_hashtags')
+    path('api/search', HashtagSearchView.as_view(), name='search_hashtags')
 ]
