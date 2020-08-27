ee4f8d8c4a782646abfab6cbc97d6e85be52f007
Chayan Datta <chayandatta007@gmail.com>
2020-08-27 18:15:14 +0530

    URL established in routes params

--- activity/urls.py
+++ activity/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from activity.api.views import PostCommentView, PostLikeView, PostReportView
 
 urlpatterns = [
-    path('api/comment/', PostCommentView.as_view(), name='comment'),
-    path('api/like/', PostLikeView.as_view(), name='like'),
-    path('api/report/', PostReportView.as_view(), name='report'),
+    path('api/comment/<post_id>', PostCommentView.as_view(), name='comment'),
+    path('api/like/<post_id>', PostLikeView.as_view(), name='like'),
+    path('api/report/<post_id>', PostReportView.as_view(), name='report'),
 ]
