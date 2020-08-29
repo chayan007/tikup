cc770a66fb3c7870194997f46e53f9f9149a8bea
Chayan Datta <chayandatta007@gmail.com>
2020-08-29 21:44:20 +0530

    Added to admin for recon

--- favorites/admin.py
+++ favorites/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from favorites.models import FavoritePost, FavoriteSound
+
+
+admin.site.register(FavoritePost)
+admin.site.register(FavoriteSound)
