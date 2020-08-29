b840a53242a26d55c39d264e6eebc75998ec28a2
Chayan Datta <chayandatta007@gmail.com>
2020-08-29 19:18:18 +0530

    Location: get_ip_details

--- base/utils.py
+++ base/utils.py
@@ -1,4 +1,6 @@
 """Get all base utils required for this project."""
+import ipinfo
+from django.conf import settings
 
 
 def get_ip_address(request):
@@ -9,3 +11,14 @@ def get_ip_address(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+
+def get_ip_details(ip_address=None):
+    """Get location based information from IP Address."""
+	ip_info_token = getattr(settings, "IPINFO_TOKEN", None)
+	ip_info_settings = getattr(settings, "IPINFO_SETTINGS", {})
+    ip_data = ipinfo.getHandler(
+        ip_info_token,
+        **ip_info_settings
+    ).getDetails(ip_address)
+    return ip_data
