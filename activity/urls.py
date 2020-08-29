deda9f47bdc5bbef1820d7985263d8cda375bda2
Chayan Datta <chayandatta007@gmail.com>
2020-08-29 22:34:25 +0530

    Reply to a post exposed

--- activity/urls.py
+++ activity/urls.py
@@ -1,10 +1,11 @@
 """Common URLs for posts application."""
 from django.urls import path
 
-from activity.api.views import PostCommentView, PostLikeView, PostReportView
+from activity.api.views import PostCommentView, PostLikeView, PostReportView, PostReplyView
 
 urlpatterns = [
     path('api/comment/<post_id>', PostCommentView.as_view(), name='comment'),
+    path('api/reply/<comment_id>', PostReplyView.as_view(), name='reply'),
     path('api/like/<post_id>', PostLikeView.as_view(), name='like'),
     path('api/report/<post_id>', PostReportView.as_view(), name='report'),
 ]
