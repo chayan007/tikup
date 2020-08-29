15a78534c479c88ae53072544f438a33b828bd70
Chayan Datta <chayandatta007@gmail.com>
2020-08-29 17:49:50 +0530

    pagination added

--- settings/base.py
+++ settings/base.py
@@ -72,7 +72,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
 }
 
 TEMPLATES = [
