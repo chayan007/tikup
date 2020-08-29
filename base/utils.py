f76e139db5ab9bc925ffcb003c3164cac7dc7878
Chayan Datta <chayandatta007@gmail.com>
2020-08-29 19:12:19 +0530

    get_ip_address done

---
+++ base/utils.py
@@ -0,0 +1,11 @@
+"""Get all base utils required for this project."""
+
+
+def get_ip_address(request):
+    """Get IP address from request headers."""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
