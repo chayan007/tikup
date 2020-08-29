d1e2503ced679ee34bfebb91d9de9f795c253b10
Chayan Datta <chayandatta007@gmail.com>
2020-08-29 21:02:19 +0530

    Favorite models

--- favorites/models.py
+++ favorites/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+from base.models import BaseModel
+
+from posts.models import Post
+
+from sounds.models import Sound
+
+from usermodule.models import Profile
+
+
+class FavoriteSound(BaseModel):
+    """Mark all favorite songs of an user."""
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    sound = models.ForeignKey(Sound, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Representation."""
+        return '{} has marked {} as favorite'.format(
+            self.profile.user.username,
+            self.sound.name
+        )
+
+
+class FavoritePost(BaseModel):
+    """Mark all favorite posts of an user."""
+
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Representation."""
+        return '{} has marked {} as favorite'.format(
+            self.profile.user.username,
+            self.post.description[:20]
+        )
