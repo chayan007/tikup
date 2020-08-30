b4b6a2322cdf5b47d76e78cd2abde18c31536640
Chayan Datta <chayandatta007@gmail.com>
2020-08-30 13:54:15 +0530

    Comment Admin

--- activity/admin.py
+++ activity/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from activity.models import Activity
+from activity.models import Activity, Comment
 
 admin.site.register(Activity)
+admin.site.register(Comment)
