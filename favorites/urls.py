859484831ee7f60f1d33a216254a54602b129396
Chayan Datta <chayandatta007@gmail.com>
2020-08-30 14:15:46 +0530

    API exposed

--- favorites/urls.py
+++ favorites/urls.py
@@ -1,9 +1,10 @@
 """Common URLs for favorites application."""
 from django.urls import path
 
-from favorites.api.views import FavoriteView
+from favorites.api.views import FavoriteView, UserInterestView
 
 urlpatterns = [
     path('api/<model_marker>/', FavoriteView.as_view(), name='list'),
     path('api/<model_marker>/<object_id>', FavoriteView.as_view(), name='mark'),
+    path('api/interest/', UserInterestView.as_view(), name='interest'),
 ]
