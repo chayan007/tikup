1eabb4cc7a68a3bbeb7e03f4b522ceb60dd06d7e
Chayan Datta <chayandatta007@gmail.com>
2020-09-01 12:50:08 +0530

    API exposed

--- posts/urls.py
+++ posts/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path, include
 
 from posts.api import router
-from posts.api.views import PostUploadView, PostSearchView
+from posts.api.views import PostUploadView, PostSearchView, PostMetricsView
 
 urlpatterns = [
     path('api/models/', include(router.urls)),
     path('api/upload/', PostUploadView.as_view(), name='upload_post'),
-    path('api/search/', PostSearchView.as_view(), name='search_post')
+    path('api/search/', PostSearchView.as_view(), name='search_post'),
+    path('api/metrics/<indicator>/<post_id>', PostMetricsView.as_view(), name='post_metrics')
 ]
