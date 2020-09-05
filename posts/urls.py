a3bbf9db4ffcbe8acd61522ad7d23806d519844d
Chayan Datta <chayandatta007@gmail.com>
2020-09-05 19:00:48 +0530

    API exposed

--- posts/urls.py
+++ posts/urls.py
@@ -2,11 +2,17 @@
 from django.urls import path, include
 
 from posts.api import router
-from posts.api.views import PostUploadView, PostSearchView, PostMetricsView
+from posts.api.views import (
+    SoundBasedPostView,
+    PostUploadView,
+    PostSearchView,
+    PostMetricsView
+)
 
 urlpatterns = [
     path('api/models/', include(router.urls)),
     path('api/upload/', PostUploadView.as_view(), name='upload_post'),
     path('api/search/', PostSearchView.as_view(), name='search_post'),
-    path('api/metrics/<indicator>/<post_id>', PostMetricsView.as_view(), name='post_metrics')
+    path('api/metrics/<indicator>/<post_id>', PostMetricsView.as_view(), name='post_metrics'),
+    path('api/sound/<sound_id>', SoundBasedPostView.as_view(), name='post_by_sound')
 ]
