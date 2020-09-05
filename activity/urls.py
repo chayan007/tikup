d1e170cd048acac74d6a28901753b1af5722678a
Chayan Datta <chayandatta007@gmail.com>
2020-09-05 20:50:14 +0530

    URL exposed

--- activity/urls.py
+++ activity/urls.py
@@ -2,6 +2,8 @@
 from django.urls import path
 
 from activity.api.views import (
+    IncrementPostView,
+    IncrementSoundView,
     LikeCommentView,
     PostCommentView,
     PostLikeView,
@@ -15,4 +17,6 @@ urlpatterns = [
     path('api/reply/<comment_id>', PostReplyView.as_view(), name='reply'),
     path('api/like/<post_id>', PostLikeView.as_view(), name='like'),
     path('api/report/<post_id>', PostReportView.as_view(), name='report'),
+    path('api/view/sound/<sound_id>', IncrementSoundView.as_view(), name='sound_view'),
+    path('api/view/post/<post_id>', IncrementPostView.as_view(), name='post_view'),
 ]
