7c922c47d4ef503a24893c94563b0540f4a26a04
Chayan Datta <chayandatta007@gmail.com>
2020-09-05 19:43:11 +0530

    Added migrations

--- favorites/admin.py
+++ favorites/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from favorites.models import FavoritePost, FavoriteSound
+from favorites.models import FavoritePost, FavoriteSound, UserInterest
 
 
 admin.site.register(FavoritePost)
 admin.site.register(FavoriteSound)
+admin.site.register(UserInterest)
