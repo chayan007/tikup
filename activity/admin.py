a8d234a8ca145522f1d731f2f55a10f2952f7db1
Chayan Datta <chayandatta007@gmail.com>
2020-09-05 18:33:07 +0530

    Comment Like added to Admin

--- activity/admin.py
+++ activity/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from activity.models import Activity, Comment
+from activity.models import Activity, Comment, CommentLike
 
 admin.site.register(Activity)
 admin.site.register(Comment)
+admin.site.register(CommentLike)
