8cf0f11bb0eeb1f62622561daa8c45ba9011567d
Chayan Datta <chayandatta007@gmail.com>
2020-09-09 22:37:06 +0530

    User interest placement changed

--- favorites/urls.py
+++ favorites/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from favorites.api.views import FavoriteView, UserInterestView
 
 urlpatterns = [
+    path('api/interest/', UserInterestView.as_view(), name='interest'),
     path('api/<model_marker>/', FavoriteView.as_view(), name='list'),
     path('api/<model_marker>/<object_id>', FavoriteView.as_view(), name='mark'),
-    path('api/interest/', UserInterestView.as_view(), name='interest'),
 ]
