b496ff7e6e4e24c099ca88644b9f6e58f6be0bcf
Chayan Datta <chayandatta007@gmail.com>
2020-09-12 14:34:07 +0530

    AudioConverter __init__

---
+++ sounds/controllers/converter.py
@@ -0,0 +1,9 @@
+"""Convert video to audio instance."""
+
+
+class AudioConverter:
+    """Convert to audio."""
+
+    def __init__(self):
+        """Declare all class variables."""
+        self.file_name = ''
