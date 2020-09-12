1907c4d396e7946532fa868c9151ef49fef30ab6
Chayan Datta <chayandatta007@gmail.com>
2020-09-12 10:39:45 +0530

    Return like, share and comments count in posts

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from posts.utils import comments_count, likes_count, share_count
+
 from sounds.api.serializers import SoundSerializer
 
 from usermodule.api.serializers import ProfileSerializer
@@ -20,7 +22,23 @@ class PostSerializer(serializers.ModelSerializer):
     profile = ProfileSerializer()
     sound = SoundSerializer()
     category = PostCategory()
+    likes = serializers.SerializerMethodField()
+    comments = serializers.SerializerMethodField()
+    shares = serializers.SerializerMethodField()
+
+    def get_likes(self, obj):
+        return likes_count(obj)
+
+    def get_comments(self, obj):
+        return comments_count(obj)
+
+    def get_shares(self, obj):
+        return share_count(obj)
 
     class Meta:
         model = Post
-        fields = ('uuid', 'profile', 'sound', 'video_file', 'video_gif', 'description', 'category')
+        fields = (
+            'uuid', 'profile', 'sound', 'video_file',
+            'video_gif', 'description', 'category',
+            'likes', 'shares', 'comments'
+        )
