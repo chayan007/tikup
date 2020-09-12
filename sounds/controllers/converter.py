735dc2518386fa89b8a093ae28c656c7159a991c
Chayan Datta <chayandatta007@gmail.com>
2020-09-12 14:42:04 +0530

    audio extractor

--- sounds/controllers/converter.py
+++ sounds/controllers/converter.py
@@ -1,4 +1,6 @@
 """Convert video to audio instance."""
+import os
+import pipes
 
 
 class AudioConverter:
@@ -7,3 +9,24 @@ class AudioConverter:
     def __init__(self):
         """Declare all class variables."""
         self.file_name = ''
+
+    @staticmethod
+    def audio_extractor(file_name):
+        try:
+            file, file_extension = os.path.splitext(file_name)
+            file = pipes.quote(file)
+            video_to_wav = 'ffmpeg -i {}{} {}.wav'.format(
+                file,
+                file_extension,
+                file
+            )
+            final_audio = 'lame {}.wav {}.mp3'.format(
+                file,
+                file
+            )
+            os.system(video_to_wav)
+            os.system(final_audio)
+            # file=pipes.quote(file)
+            # os.remove(file + '.wav')
+        except OSError as err:
+            print(str(err))
