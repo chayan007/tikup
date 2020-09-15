b457f994bd2308ef20eb38679f0eed20149bc065
Chayan Datta <chayandatta007@gmail.com>
2020-09-15 23:56:10 +0530

    Sound extractor to save flow

--- sounds/controllers/converter.py
+++ sounds/controllers/converter.py
@@ -1,6 +1,12 @@
 """Convert video to audio instance."""
 import os
 import pipes
+import time
+
+from django.core.files import File
+
+from posts.models import Post
+from sounds.models import Sound
 
 
 class AudioConverter:
@@ -26,7 +32,26 @@ class AudioConverter:
             )
             os.system(video_to_wav)
             os.system(final_audio)
-            # file=pipes.quote(file)
-            # os.remove(file + '.wav')
+            return '{}.mp3'.format(file)
         except OSError as err:
             print(str(err))
+
+    def _add_to_sound(self, file_name, profile):
+        """Store audio to Sound model."""
+        with open(file_name, 'rb') as fi:
+            Sound.objects.create(
+                profile=profile,
+                sound_file=File(fi, name=os.path.basename(fi.name)),
+                is_extracted_audio=True
+            )
+
+    def extract(self, post_uuid):
+        """Extract audio from one post."""
+        post = Post.objects.get(uuid=post_uuid)
+        file_name = post.video_file.path
+        audio_file_path = self.audio_extractor(file_name)
+        time.sleep(1 * 60)
+        self._add_to_sound(
+            audio_file_path,
+            post.profile
+        )
