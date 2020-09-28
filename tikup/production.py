72bf6db4f8ae95cddd19a19fd0685c9f90391b9c
AdityaBhalsod <adityabhalsod8@gmail.com>
2020-09-28 23:17:13 +0530

    Change the object of producation database.

--- tikup/production.py
+++ tikup/production.py
@@ -7,10 +7,10 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
         'PORT': '3306',
     }
 }
